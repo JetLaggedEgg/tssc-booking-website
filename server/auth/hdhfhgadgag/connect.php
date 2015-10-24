482c4855184fa0dde07e75f0e82a77b11655d840
Daniel Sarracayo <daniel.sarracayo@gmail.com>
2015-10-24 12:03:58 +0100

    Password Clarity.

--- server/auth/hdhfhgadgag/connect.php
+++ server/auth/hdhfhgadgag/connect.php
@@ -1,4 +1,4 @@
 <?php
-	mysql_connect("localhost", "root", "Donkeysmelon12");
+	mysql_connect("localhost", "root", "Donkeysmelon12"); // Filler password by the way.
 	mysql_select_db("it_tssc");
 ?>
